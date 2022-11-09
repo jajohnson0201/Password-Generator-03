06281132ec7de0d3fe3c9d4b6996b32d6b2bdeec
joe <jajohnson0201@gmail.com>
2022-11-09 13:20:40 -0600

    added a random string maker and window alert with answer, removed commas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var  password= generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
@@ -23,11 +23,12 @@ function generatePassword(){
    var lowercaseLetters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
    var uppercaseLetters= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
    var passSelection= ("");
+   
 
   var passLength = window.prompt("enter a length of password desired between 8 and 128 characters");
   
-  while (passLength <= 8 ||
-    passLength >= 128) { 
+  while (passLength < 8 ||
+    passLength > 128) { 
       passLength = window.prompt("enter a length of password desired between 8 and 128 characters");
     }
 console.log(passLength);
@@ -67,10 +68,65 @@ console.log(passLength);
   window.alert("generating your password");
 
   console.log(passSelection);
-
+  console.log(typeof passSelection);
+  passSelection = passSelection.replaceAll(",","");
   
+  function generateP() {
+    var p = '';
+    for (var x = 0; x < passLength; x ++){
+      p += passSelection[Math.floor(Math.random() * passSelection.length)];
+      console.log(p);
+    } 
 
-  
+    console.log("Password String: " + p);
+    console.log(typeof p);
+    
+    return p;
+  }
+  var password = generateP();
+  window.alert("This is your randomly generated password: " + password);
+  console.log(generateP());
+
+
+  /*
+  let varGlobal = 5;
+
+  function noParams() {
+    let varA = 10;
+
+    let result = varGlobal + varA;
+    result = withParams(result);
+    return [result, varA, 9, "Bingo"];
+  }
+
+  var testing = noParams()
+
+  function withParams(anything) {
+    let newResult = 5 + anything;
+    return newResult;
+  }
+
+
+  // We define the function
+  function name(parameter_1, parameter_2) {
+    console.log(parameter_1)
+    console.log(parameter_2);
+    let test = parameter_1;
+
+    return function nestedFunction() {
+      console.log(test)
+    }
+  }
+
+  // here we invoke/call the function
+  name("Bingo", "Chicken");
+  let result = name(5, 2);
+  result();
+
+*/
+
+
+
   /*
     function(specialcharconf):{
       for (var x =0; x < passLength; x++){
